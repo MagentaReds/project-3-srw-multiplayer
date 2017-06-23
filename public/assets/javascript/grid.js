c38dc0b54ecf0de5430eeecc686bd40544d0b7b1
Jacob Wallace <jacob.wallace@utexas.edu>
2017-06-22 21:30:55 -0500

    started registering row/column data for pathfinding

--- public/assets/javascript/grid.js
+++ public/assets/javascript/grid.js
@@ -1,5 +1,15 @@
-for (var y = 29; y >= 0; y--) {
-	for (var x = 0; x < 30; x++) {
-		$("#selectable").append(`<li class="ui-state-default" data-x = "${x}" data-y = "${y}"></li>`);
+for (var r = 0; r < 30; r++) {
+	for (var c = 0; c < 30; c++) {
+		$("#grid").append(`<li class="ui-state-default" data-r = "${r}" data-c = "${c}"></li>`);
 	}
 }
+
+$(document).on("mousedown", "li", function(event) {
+   var dataR = $(this).attr("data-r");
+   var dataC = $(this).attr("data-c");
+   var R = parseInt(dataR) - 1;
+   $(`li[data-r=${R}][data-c=${dataC}]`).html("^");
+   console.log("r: " + dataR);
+   console.log("c: " + dataC);
+   console.log("====");
+});
