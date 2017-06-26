1681b2e457b24fd5a0eba8a295984befa23f6740
MagentaReds <grantodaniels@gmail.com>
2017-06-26 03:32:12 -0500

    Changed findById to findOne and using a filter

--- config/gameConfig.js
+++ config/gameConfig.js
@@ -12,16 +12,16 @@ module.exports = function() {
 
   var pilot1, pilot2, mech1, mech2;
 
-  return dbPilot.findById("594f51f9fcea503080e0ae66")
+  return dbPilot.findOne({name: "Kyosuke Nambu"})
   .then(function(pilot){
     pilot1=pilot;
-     return dbMech.findById("594f51f9fcea503080e0afba");
+     return dbMech.findOne({name: "Alteisen"}).populate("Weapon");
   }).then(function(mech){
     clientList[0].units.push(new Unit(pilot1, mech));
-    return dbPilot.findById("594f51f9fcea503080e0ae67");
+    return dbPilot.findOne({name: "Excellen Browning"});
   }).then(function(pilot){
     pilot2=pilot;
-    return dbMech.findById("594f51f9fcea503080e0afe7")
+    return dbMech.findOne({name: "Weissritter"}).populate("Weapon")
   }).then(function(mech){
     clientList[1].units.push(new Unit(pilot2, mech));
     return new Promise(function (resolve, reject) {
