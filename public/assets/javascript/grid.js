9d710fec206063f1951298b266443be890110b7e
Jacob Wallace <jacob.wallace@utexas.edu>
2017-06-27 15:21:28 -0500

    Started seperating functions out for code readability, fixed multipble binding bug

--- public/assets/javascript/grid.js
+++ public/assets/javascript/grid.js
@@ -1,13 +1,18 @@
 var grid = [];
+var gridCoordinates = [];
 
 $("#menu").hide();
+$("#cancel").hide();
 
 // code that makes 900 grid-tiles with each row and column's data index stored
 for (var r = 0; r < 30; r++) {
 	for (var c = 0; c < 30; c++) {
-		$("#grid").append(`<li class="grid-square" data-r = "${r}" data-c = "${c}"></li>`);
+		$("#grid").append(`<li class="blink" class="grid-square" data-r = "${r}" data-c = "${c}"></li>`);
+		gridCoordinates.push([r,c]);
 	}
 }
+
+$("li").toggleClass('blink');
 
 for (var i = 0; i < 30; i++) {
 	grid.push([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]);
@@ -53,6 +58,73 @@ var availableMoveSpaces = [
 
 ];
 
+function displayActiveTile(locate) {
+	// locates active tile where unit will be and colors in tile with green
+	$(`li[data-r=${locate[0]}][data-c=${locate[1]}]`).css('background', "#64dd17").css('opacity', "0.5");
+}
+function blinkActiveTile(locate) {
+	// locates active tile where unit will be and blinks tile, seperate function because we will want to keep coloring in active tile while not toggling the blink
+	$(`li[data-r=${locate[0]}][data-c=${locate[1]}]`).toggleClass('blink');
+}
+// call these functions on load
+displayActiveTile(activeUnit);
+blinkActiveTile(activeUnit);
+activeUnitFunctionality(activeUnit);
+
+function findActivePlayer() {
+	// if statement here that checks socket.io data to see which players turn it is
+	// also for testing purposes, binding menu buttons here as well
+	$("#move").bind("click", moveUnit);
+}
+findActivePlayer();
+
+function displayAvailableMoveTiles(locate) {
+	// locates and loops through available tiles that active unit can move to, coloring them blue and toggling the CSS blink class
+	for (var y = 0; y < availableMoveSpaces.length; y++) {
+		$(`li[data-r=${locate[y][0]}][data-c=${locate[y][1]}]`).css('background', "#2196f3").css('opacity', "0.5").toggleClass('blink').bind("click", moveTiles);
+	}
+}
+
+function hideAvailableMoveTiles(locate) {
+	// function that is called by the cancel button to hide and unbind click event available move tiles
+	for (var y = 0; y < availableMoveSpaces.length; y++) {
+		$(`li[data-r=${locate[y][0]}][data-c=${locate[y][1]}]`).css('background', "transparent").css('opacity', "1").toggleClass('blink').unbind("click");
+	}
+}
+
+function moveTiles() {
+	// Each available move tile from the above function will bind to this function
+	// clicked tile data will need to be sent to server
+	console.log("Move Action sent to server");
+	console.log([parseInt($(this).attr("data-r")),parseInt($(this).attr("data-c"))]);
+}
+
+function moveUnit (e) {
+	// function that will be bound to JQuery UI's menu button with the "move" label.
+	displayAvailableMoveTiles(availableMoveSpaces);
+	$("#cancel").show().bind("click", cancelMove);
+}
+
+function cancelMove (e) {
+	hideAvailableMoveTiles(availableMoveSpaces);
+	$("#cancel").hide();
+}
+
+function activeUnitFunctionality(locate) {
+	$(`li[data-r=${locate[0]}][data-c=${locate[1]}]`).on("click", function(){
+		$("#menu").show();
+		var dataR = $(this).attr("data-r");
+		var dataC = $(this).attr("data-c");
+		$(document).on("click", "li", function(event) {
+			// if next tile clicked is outside the one that was previously clicked on
+			if (($(this).attr("data-r")!=dataR) || ($(this).attr("data-c")!=dataC)){
+				$("#menu").hide();
+			}
+		});
+	});
+}
+// displayAvailableMoveTiles(availableMoveSpaces);
+
 // code to display active unit
 // from server we will get an array where the active unit on current turn is located
 // in our example case, we have set active unit at [5,5] which is what we set as the argument
@@ -60,10 +132,9 @@ var availableMoveSpaces = [
 // when clicking on this active unit grid tile, UI should pop up that shows we can either
 // move or attack with unit. Availabe move spaces will also be sent in from the server as an array of arrays.
 // in our example case we set those in availableMoveSpaces variable
-function displayActiveUnit(locate) {
-	(function blink() {
-		$(`li[data-r=${locate[0]}][data-c=${locate[1]}]`).css('background', "#64dd17").css('opacity', "0.5").fadeOut(500).fadeIn(500, blink);
-	})();
+function activeUnitFunctionalityTest(locate) {
+
+	// $(`li[data-r=${locate[0]}][data-c=${locate[1]}]`).css('background', "#64dd17").css('opacity', "0.5");
 	// below code will display tiles where player is able to move unit
 	$(`li[data-r=${locate[0]}][data-c=${locate[1]}]`).on("click", function(){
 		$("#menu").show();
@@ -76,24 +147,98 @@ function displayActiveUnit(locate) {
 				$("#menu").hide();
 			}
 		})
+		// moving tiles
+		// for (var y = 0; y < availableMoveSpaces.length; y++) {
+		// 	$(`li[data-r=${availableMoveSpaces[y][0]}][data-c=${availableMoveSpaces[y][1]}]`).bind("click", moveTiles);
+		// }
+		// function moveTiles (e) {
+		// 	if (moveUnit) {
+		// 	console.log("Move Action sent to server");
+		// 	console.log([parseInt($(this).attr("data-r")),parseInt($(this).attr("data-c"))]);
+		// }
+		// }
 		$("#move").bind("click", moveUnit);
 			//$(document).on("click", "li", function(event) {
 			function moveUnit (e) {
+				displayAvailableMoveTiles(availableMoveSpaces);
 
-				for (var y = 0; y < availableMoveSpaces.length; y++) {
-					$(`li[data-r=${availableMoveSpaces[y][0]}][data-c=${availableMoveSpaces[y][1]}]`).bind("click", moveTiles);
-				}
+				// var moveUnit = true;
+				$("#cancel").show();
+				//
+				$("#cancel").on("click", function() {
+					for (var z = 0; z < availableMoveSpaces.length; z++) {
+						$(`li[data-r=${availableMoveSpaces[z][0]}][data-c=${availableMoveSpaces[z][1]}]`).css('background', "transparent").css('opacity', "1");
+					}
+				// 	blinkMoveTiles();
+				// 	$("#cancel").hide();
+				// 	moveUnit = false;
+				// })
 
-				function moveTiles (e) {
-					console.log("Move Action sent to server");
-					console.log([parseInt($(this).attr("data-r")),parseInt($(this).attr("data-c"))]);
-				}
+				// for (var y = 0; y < availableMoveSpaces.length; y++) {
+				// 	$(`li[data-r=${availableMoveSpaces[y][0]}][data-c=${availableMoveSpaces[y][1]}]`).bind("click", moveTiles);
+				// }
+				var xyz = 0;
+				// for (var f = 0; f < gridCoordinates.length; f++) {
+				// 	//var arrayCheck = [gridCoordinates[y][0],gridCoordinates[y][1]];
+				// 	//console.log(availableMoveSpaces[y]);
+				// 		if ((availableMoveSpaces[xyz][0] === gridCoordinates[f][0]) && (availableMoveSpaces[xyz][1] === gridCoordinates[f][1])) {
+				// 		//console.log([gridCoordinates[y][0],gridCoordinates[y][1]]);
+				// 		//console.log(gridCoordinates[y]);
+				// 		console.log([availableMoveSpaces[xyz][0],availableMoveSpaces[xyz][1]])
+				// 			console.log("hi");
+				// 			xyz++;
+				// 		}
+				// 		else {
+				// 			$(`li[data-r=${gridCoordinates[f][0]}][data-c=${gridCoordinates[f][1]}]`).bind("click", outsideMoveTiles);
+				// 		}
+				// 	}
+					//}
+					// else {
+					// 	$(`li[data-r=${grid[y][0]}][data-c=${grid[y][1]}]`).bind("click", outsideMoveTiles);
+					// 	//console.log($(`li[data-r=${grid[y][0]}][data-c=${grid[y][1]}]`));
+					// }
+				//}
 
-				(function blink() {
-				for (var y = 0; y < availableMoveSpaces.length; y++) {
-					$(`li[data-r=${availableMoveSpaces[y][0]}][data-c=${availableMoveSpaces[y][1]}]`).css('background', "#2196f3").css('opacity', "0.5").fadeOut(500).fadeIn(500, blink);
+				// function moveTiles (e) {
+				// 	if (moveUnit) {
+				// 	console.log("Move Action sent to server");
+				// 	console.log([parseInt($(this).attr("data-r")),parseInt($(this).attr("data-c"))]);
+				// }
+				// }
+
+				function outsideMoveTiles (e) {
+					// for (var y = 0; y < availableMoveSpaces.length; y++) {
+					// 	$(`li[data-r=${availableMoveSpaces[y][0]}][data-c=${availableMoveSpaces[y][1]}]`).css('background', "transparent").css('opacity', "1").toggleClass('blink');
+					// }
+					console.log("Can't move there! outside!");
+					// $("li").css('background', "transparent");
+					// setTimeout(function(){
+					// 	displayActiveUnit(activeUnit);
+					// }, 500);
+					// var moveNotAllowed;
+					// for (var x = 0; x < availableMoveSpaces.length; x++) {
+					// 	if (availableMoveSpaces.includes([parseInt($(this).attr("data-r")),parseInt($(this).attr("data-c"))])) {
+					// 		console.log("hi");
+					// 	}
+					// 	else {
+					// 		moveNotAllowed.push(availableMoveSpaces);
+					// 	}
+					// }
 				}
-				})();
+				//while (moveClicked == true) {
+				function blinkMoveTiles () {
+					for (var y = 0; y < availableMoveSpaces.length; y++) {
+						$(`li[data-r=${availableMoveSpaces[y][0]}][data-c=${availableMoveSpaces[y][1]}]`).toggleClass('blink');
+					}
+				}
+				function colorMoveTiles() {
+					for (var y = 0; y < availableMoveSpaces.length; y++) {
+						$(`li[data-r=${availableMoveSpaces[y][0]}][data-c=${availableMoveSpaces[y][1]}]`).css('background', "#2196f3").css('opacity', "0.5");
+					}
+				}
+				// colorMoveTiles();
+				// blinkMoveTiles();
+			//}
 				// function bindThis() {
 				// 	if (($(this).attr("data-r")!=dataR) || ($(this).attr("data-c")!=dataC)){
 				// 		console.log("hi");
@@ -107,12 +252,13 @@ function displayActiveUnit(locate) {
 				// 	// 	console.log(moveSpaceCheckArray);
 				// 	// }
 				// 	})
-			}
+			})
 //})
-	});
-};
+	};
+});
+}
 
-displayActiveUnit(activeUnit);
+//activeUnitFunctionality(activeUnit);
 
 // code for seeing which tile was clicked on
 // also the UI for displaying options for clicked grid square should pop up here
@@ -124,6 +270,7 @@ $(document).on("click", "li", function(event) {
 		// if next clicked tile is outside the one that was previously clicked on
 		if (($(this).attr("data-r")!=dataR) || ($(this).attr("data-c")!=dataC)){
 			$(`li[data-r=${dataR}][data-c=${dataC}]`).css('background', "transparent").css('opacity', "1");
+			displayActiveTile(activeUnit);
 		}
 	});
 });
