5fef74f381a7f900f19525360ab5d1f50a021dee
MagentaReds <grantodaniels@gmail.com>
2017-07-02 13:06:46 -0500

    Added getIndexArr helper function.

--- config/helpers.js
+++ config/helpers.js
@@ -7,6 +7,17 @@ var helpers = {
     var a = JSON.stringify(arr1);
     var b = JSON.stringify(arr2);
     return a.indexOf(b) != -1;
+  },
+
+  //returns index where arr2 first matches arr1[i], assumes arr1 is an array of length 2 arrays, and arr2 length is 2
+  getIndexArr: function(arr1, arr2) {
+    for(var i=0; i<arr1.length; ++i){
+      if(arr1[i][0]===arr2[0] && arr1[i][1]===arr2[1]) {
+        return i;
+      }
+    }
+
+    return -1;
   }
 }
 
