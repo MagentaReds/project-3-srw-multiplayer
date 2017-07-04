43a1f503f8f722f80766a04fd6f1db1145ae4e53
MagentaReds <grantodaniels@gmail.com>
2017-07-04 03:43:07 -0500

    Added new method terPer that returns the scaler of the terrain performace for a given terrain type.

--- game/weapon.js
+++ game/weapon.js
@@ -43,6 +43,36 @@ class Weapon {
   isMap() {
     return this.props.includes("MAP");
   }
+
+  terPer(terrain) {
+    var index=-1;
+    switch(terrain) {
+      case "Air":
+        index=0;
+        break;
+      case "Grd":
+        index=1;
+        break;
+      case "Wtr":
+        index=2;
+        break;
+      case "Spc":
+        index=3;
+        break;
+    }
+    switch(this.terrain.charAt(index)) {
+      case 'S':
+        return 1.1;
+      case 'A':
+        return 1.0;
+      case 'B':
+        return .9;
+      case 'C':
+        return .8;
+      case 'D':
+        return .4;
+    }
+  }
 }
 
 module.exports=Weapon;
