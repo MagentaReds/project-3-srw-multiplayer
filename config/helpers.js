c710b8ed24150fbbe28ebada8eba0a580cc79538
MagentaReds <grantodaniels@gmail.com>
2017-07-08 04:54:04 -0500

    Removed makeUnitsFromTeam function and put it in its own file.  This fixes some errors with cross requires.

--- config/helpers.js
+++ config/helpers.js
@@ -1,13 +1,3 @@
-var Unit = require("../game/unit.js");
-
-var dbUser = require("../models/user.js");
-var dbTeam = require("../models/team.js");
-var dbPilot = require("../models/pilot.js");
-var dbMech = require("../models/mech.js");
-var dbWeapon = require("../models/weapon.js");
-
-var Promise = require("bluebird");
-
 //Just some helper functions
 
 var helpers = {
@@ -30,7 +20,7 @@ var helpers = {
     return -1;
   },
 
-  getPrevailHitEvdArm(level, percentHp) {
+  getPrevailHEA: function(level, percentHp) {
     if(!level)
       return 0;
 
@@ -49,7 +39,7 @@ var helpers = {
     return table[level][temp];
   },
 
-  getPrevailCrit(level, percentHp) {
+  getPrevailCrit: function(level, percentHp) {
     if(!level)
       return 0;
 
@@ -68,15 +58,15 @@ var helpers = {
     return table[level][temp];
   },
 
-  getTKHitEvd(level) {
+  getTKHitEvd: function(level) {
     return (level-1)*3;
   },
 
-  getTKField(level) {
+  getTKField: function(level) {
     return level*200;
   },
 
-  getFightDmg(level) {
+  getFightDmg: function(level) {
     if(level>=1 && level <=3)
       return level*50;
     else if(level>=4 && level<=6)
@@ -85,41 +75,6 @@ var helpers = {
       return (level-2)*50;
   },
 
-  makeUnitsFromTeam(userId) {
-    console.log(userId);
-    return new Promise(function(resolve, reject) {
-      var client= {
-        
-      };
-      var units=new Array(6);
-      dbUser.findById(userId, function(err, res){
-        if(err) return reject();
-        //console.log(res);
-        dbTeam.findOne({number: res.team}, function(err, res2){
-          //console.log(res2);
-          var promi = [];
-          for(let i=0; i<6; ++i){
-            promi.push(dbPilot.findOne({name: res2.units[i].pilotName}));    
-          }
-          Promise.all(promi).then(function(pilotVals) {
-            var promi2=[];
-            for(let i=0; i<6; ++i)
-              promi2.push(dbMech.findOne({name:res2.units[i].mechName}).populate("weapons iWeapons").exec());
-            Promise.all(promi2).then(function(mechVals){
-              for(let i=0; i<6; ++i){
-                units[i]=new Unit(userId, pilotVals[i], mechVals[i]);
-              }
-              client.id=res._id;
-              client.name=res.username;
-              client.units=units;
-              resolve(client);
-            });
-          });
-          
-        })
-      })
-    });
-  }
 }
 
 module.exports = helpers;
